3b9bf9a505f919f543ca41e671099e1610e16d1a
Alex Alves <aalves.dev@gmail.com>
2024-01-10 01:59:03 +0000

    Quartz sync: Jan 10, 2024, 1:59 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Vigilante",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "alexd-alves.github.io/vigilante",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
